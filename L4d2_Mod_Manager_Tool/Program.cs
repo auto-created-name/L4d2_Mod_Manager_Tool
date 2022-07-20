00bcc8d5d34afcb4940336b49760691c4841edb2
saint_louis <saint_louis_down@163.com>
2022-07-20 23:21:57 +0800

    安装 Facepunch.SteamWork 2.3.2 ; 编写最基础创意工坊服务

--- L4d2_Mod_Manager_Tool/Program.cs
+++ L4d2_Mod_Manager_Tool/Program.cs
@@ -14,6 +14,8 @@ namespace L4d2_Mod_Manager_Tool
         [STAThread]
         static void Main()
         {
+            var i = Service.WorkshopItemService.Instance;
+            i.Map(s => s.CollectModInfo("12"));
             // ∑˛ŒÒ‘ÿ»Î
             Service.ModCategoryService.Load();
             Service.AddonListService.Load();
