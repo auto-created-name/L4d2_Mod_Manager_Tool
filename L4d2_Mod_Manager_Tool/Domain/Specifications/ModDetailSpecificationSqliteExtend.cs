5133eb2d6dd1df7e4d8307a81d4c1bc764865d83
saint_louis <saint_louis_down@163.com>
2022-08-02 17:03:52 +0800

    修复进行模糊查询时报 Invalid Operation 的错误

--- L4d2_Mod_Manager_Tool/Domain/Specifications/ModDetailSpecificationSqliteExtend.cs
+++ L4d2_Mod_Manager_Tool/Domain/Specifications/ModDetailSpecificationSqliteExtend.cs
@@ -15,6 +15,7 @@ namespace L4d2_Mod_Manager_Tool.Domain.Specifications
                 MS_Empty    => "",
                 MS_Category => $"categories like '%{(spec as MS_Category).Category}%'",
                 MS_Tag      => $"workshop_tags like '%{(spec as MS_Tag).Tag}%'",
+                MS_Blur blur => string.Format("(title like '%{0}%' OR workshop_title like '%{0}%' OR author like '%{0}%' OR vpk_id like '{0}%')", blur.Blur),
                 MS_And and  => and.Left.ToSqlite() + " AND " + and.Right.ToSqlite(),
                 MS_Or or    => $"({or.Left.ToSqlite()} OR {or.Right.ToSqlite()})",
                 _           => throw new InvalidOperationException()
